59009b04e6bbff3840c9087ba28f5dfdbce120d3
Chris Grabda <ccg5437@g.rit.edu>
2021-02-26 12:19:46 -0500

    created startup file to prompt the user for necessary information and create a config file

---
+++ startup.py
@@ -0,0 +1,41 @@
+'''
+Written by Christopher Grabda
+
+Creates or overwrites config.ini file
+Takes user input construct settings
+'''
+def ynInput(message):
+    while(True):
+        try:
+            userInput = input(message)
+            userInput = userInput.strip().lower()
+        except:
+            print("----------------\nInvalid Input\n----------------")
+        
+        if (userInput == "y"):
+            return "TRUE"
+        elif (userInput == "n"):
+            return "FALSE"
+    
+def pathInput():
+    print()
+    try:
+        userInput = input("Please enter your user path exactly as shown\nC:/Users/cgrab \n: ")
+        userInput = userInput.strip()
+    except:
+        print("----------------\nInvalid Input\n----------------")
+    
+    return userInput
+
+if __name__ == "__main__":
+    running = True
+    string = "[Settings]\n"
+ 
+    string += "USER_PATH: " + pathInput() +"\n"
+    string += "HAS_MSEDGE: " + ynInput("Would you like to use this on Microsoft Edge? (y/n)\n: ") + "\n"
+    string += "HAS_CHROME: " + ynInput("Would you like to use this on Google Chrome? (y/n)\n: ") + "\n"
+    string += "HAS_FFOX: " + ynInput("Would you like to use this on FireFox? (y/n)\n: ") + "\n"
+
+    with open("config.ini", "w") as config:
+        config.write(string)
+
