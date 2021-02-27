956a0ed1675f52fe618b85124e4389612e632940
Chris Grabda <ccg5437@g.rit.edu>
2021-02-26 21:08:48 -0500

    removed Firefox question because not implemented yet

--- startup.py
+++ startup.py
@@ -34,7 +34,8 @@ if __name__ == "__main__":
     string += "USER_PATH: " + pathInput() +"\n"
     string += "HAS_MSEDGE: " + ynInput("Would you like to use this on Microsoft Edge? (y/n)\n: ") + "\n"
     string += "HAS_CHROME: " + ynInput("Would you like to use this on Google Chrome? (y/n)\n: ") + "\n"
-    string += "HAS_FFOX: " + ynInput("Would you like to use this on FireFox? (y/n)\n: ") + "\n"
+    # string += "HAS_FFOX: " + ynInput("Would you like to use this on FireFox? (y/n)\n: ") + "\n"
+    string += "HAS_FFOX: False\n"
 
     with open("config.ini", "w") as config:
         config.write(string)
