adc84c81ce04186be4ad7546d0cf0fbd9b39642a
Chris Grabda <ccg5437@g.rit.edu>
2021-02-27 16:28:24 -0500

    changed startup.py to create Whitelist.ini file on run, if one does not exist

--- startup.py
+++ startup.py
@@ -4,6 +4,10 @@ Written by Christopher Grabda
 Creates or overwrites config.ini file
 Takes user input construct settings
 '''
+import os
+
+WHITELIST_STRING = "; Whitelist your own sites to the list following the format below!\n;\n; This functions to save the favicons of websites not bookmarked\n[Whitelist]\nlinks = \n    https://www.reddit.com/,\n    https://github.com/,\n    https://inloop.github.io/sqlite-viewer/,\n    https://stackoverflow.com/"
+
 def ynInput(message):
     while(True):
         try:
@@ -20,7 +24,7 @@ def ynInput(message):
 def pathInput():
     print()
     try:
-        userInput = input("Please enter your user path exactly as shown\nC:/Users/cgrab \n: ")
+        userInput = input("Please enter your user path exactly as shown\nC:/Users/cgrab \n> ")
         userInput = userInput.strip()
     except:
         print("----------------\nInvalid Input\n----------------")
@@ -28,15 +32,19 @@ def pathInput():
     return userInput
 
 if __name__ == "__main__":
-    running = True
     string = "[Settings]\n"
  
     string += "USER_PATH: " + pathInput() +"\n"
-    string += "HAS_MSEDGE: " + ynInput("Would you like to use this on Microsoft Edge? (y/n)\n: ") + "\n"
-    string += "HAS_CHROME: " + ynInput("Would you like to use this on Google Chrome? (y/n)\n: ") + "\n"
-    # string += "HAS_FFOX: " + ynInput("Would you like to use this on FireFox? (y/n)\n: ") + "\n"
+    string += "HAS_MSEDGE: " + ynInput("Would you like to use this on Microsoft Edge? (y/n)\n> ") + "\n"
+    string += "HAS_CHROME: " + ynInput("Would you like to use this on Google Chrome? (y/n)\n> ") + "\n"
+    # string += "HAS_FFOX: " + ynInput("Would you like to use this on FireFox? (y/n)\n> ") + "\n"
     string += "HAS_FFOX: False\n"
 
     with open("./resources/config.ini", "w") as config:
         config.write(string)
+    
+    # If file does not exist, creates a template Whitelist file
+    if (not os.path.exists("Whitelist.ini")):
+        with open("Whitelist.ini", "w") as whitelist:
+            whitelist.write(WHITELIST_STRING)
 
